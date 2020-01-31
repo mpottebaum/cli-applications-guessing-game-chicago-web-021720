6e0cbe58c22b614d46f90a1da6dd6c9f93fa2832
Mike Pottebaum <mpottebaum@gmail.com>
2020-01-31 00:53:29 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,7 +1,5 @@
 def run_guessing_game
   random_number = rand(6) + 1
-  puts "The computer is thinking of a number between 1 and 6"
-  puts "If you guess the number, you win"
   user_input = gets
   if user_input.chomp == "exit"
     puts "Goodbye!"
