f646da93e7aafba96717b2d3dc9915358b8ece58
Mike Pottebaum <mpottebaum@gmail.com>
2020-01-31 00:52:35 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -3,14 +3,12 @@ def run_guessing_game
   puts "The computer is thinking of a number between 1 and 6"
   puts "If you guess the number, you win"
   user_input = gets
-  binding.pry
   if user_input.chomp == "exit"
     puts "Goodbye!"
     return
   end
   
   user_number = (user_input.chomp).to_i
-  binding.pry
   if user_number == random_number
     puts "You guessed the correct number!"
   else
