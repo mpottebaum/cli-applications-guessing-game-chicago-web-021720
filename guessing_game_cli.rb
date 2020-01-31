33f99961551ea0aeae0bbfb2b74fe8a959ae251f
Mike Pottebaum <mpottebaum@gmail.com>
2020-01-31 00:45:20 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,3 +1,11 @@
 def run_guessing_game
-  random_number = rand(1..6)
+  random_number = rand(6) + 1
+  puts "I'm thinking of a number between 1 and 6"
+  puts "If you guess my number, you win"
+  user_input = gets
+  user_number = user_input.chomp
+  
+  if user_number == random_number
+    puts "You guessed the correct number!"
+  end
 end
